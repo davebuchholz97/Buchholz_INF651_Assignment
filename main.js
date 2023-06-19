47011e58bcb0dc4a32cd2f23d8946468553dca45
davebuchholz97 <136280438+davebuchholz97@users.noreply.github.com>
2023-06-19 16:56:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//Assignment 2
+//question1 and question2
+let myNumber = 5;
+
+//question3
+let myOtherNumber = 5;
+
+//question4
+let myTotal = myNumber * myOtherNumber;
+//console.log(myTotal);
+
+//question5
+let myName = "Dave";
+let myCombo = ("The sum is " + (myNumber + myOtherNumber));
+//console.log(myCombo);
+
+//question6
+let myOtherCombo = myNumber + myOtherNumber;
+//console.log(myOtherCombo);
+
+//question7
+let myRemainder = myNumber % 5;
+//console.log(myRemainder);
+
+//question8
+let i = 2; 
+//console.log(i);
+
+//question9
+let int = 4;
+//console.log(int);
+
+//question10
+const myBoolean = false;
+//console.log(myBoolean);
